5c909dc5af15ffa55dbe5097107a576ed11fb627
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-20 00:56:47 -0700

    following readme guidelines to start creating server.js with init installs as well as created db folder and files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);

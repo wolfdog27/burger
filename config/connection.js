e7ed60c9dc26dfa0d5a5287d459da9d22abb900d
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-20 20:20:01 -0700

    fixed spelling

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var mysql = require("mysql");
 
 var connection;
-if (provess.env.JAWSDB_URL){
-    connection = mysql.createConnection(provess.env.JAWSDB_URL)
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: "localhost",
